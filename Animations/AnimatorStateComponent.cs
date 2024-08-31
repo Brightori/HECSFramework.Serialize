9e753a004eae80234346b5ac665380a63c71d3e1
Brightori <el.brightori@gmail.com>
2024-08-31 03:46:32 +0300

    upd: component formatting

--- Animations/AnimatorStateComponent.cs
+++ Animations/AnimatorStateComponent.cs
@@ -4,8 +4,9 @@ using HECSFramework.Serialize;
 
 namespace Components
 {
-    [Serializable][Documentation(Doc.Animation, Doc.HECS, "This component holds serialized state of animator, you need to set the animator parameters through this component")]
-    public sealed partial  class AnimatorStateComponent : BaseComponent
+    [Serializable]
+    [Documentation(Doc.Animation, Doc.HECS, "This component holds serialized state of animator, you need to set the animator parameters through this component")]
+    public sealed partial class AnimatorStateComponent : BaseComponent
     {
         [Field(0, typeof(AnimatorStateResolver))]
         public AnimatorState State;
