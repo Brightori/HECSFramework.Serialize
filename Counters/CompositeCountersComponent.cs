7b98bfe922f12c275f7cf3183f1ff6b89a08b54c
Brightori <el.brightori@gmail.com>
2025-12-15 12:01:48 +0300

    add: composite counter

---
+++ Counters/CompositeCountersComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using HECSFramework.Core;
+
+namespace Components
+{
+    [Serializable]
+    [Documentation(Doc.HECS, Doc.Counters, "here we hold just counters without componets to add them to counters holder, we need use it if we want operate counters and resources and save them between sessions, otherwise better use counters holder directly")]
+    public class CompositeCountersComponent : BaseComponent, ISavebleComponent
+    {
+        [Field(0)]
+        public List<DefaultIntCounter> IntCounters = new List<DefaultIntCounter>(0);
+
+        [Field(1)]
+        public List<DefaultFloatCounter> FloatCounters = new List<DefaultFloatCounter>(0);
+
+        public override void AfterInit()
+        {
+            foreach (var counter in IntCounters)
+            {
+                Owner.GetComponent<CountersHolderComponent>().AddCounter(counter);
+            }
+
+            foreach (var counter in FloatCounters)
+            {
+                Owner.GetComponent<CountersHolderComponent>().AddCounter(counter);
+            }
+        }
+    }
+}
